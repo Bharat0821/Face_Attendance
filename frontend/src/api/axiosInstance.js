b74f90a704df341d8c589fb9b6dd42b2901f90b5
Bharat <swamibharat2110@gmail.com>
2026-05-09 20:26:02 +0530

    axiosChanged

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,25 +1,11 @@
 import axios from 'axios';
 
-// Use Vite's environment variable
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const API = axios.create({
   baseURL: API_BASE_URL,
 });
 
-// Request interceptor
-API.interceptors.request.use(
-  (config) => {
-    const teacher = sessionStorage.getItem('teacher');
-    if (teacher) {
-      config.headers['X-Teacher-ID'] = JSON.parse(teacher).teacherId;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Response interceptor
 API.interceptors.response.use(
   (response) => response,
   (error) => {
